40d4ce758ac48e5ade1c8e3f872e0a415d2df641
akira-cn <akira.cn@gmail.com>
2019-08-12 16:49:18 +0800

    update demos

---
+++ demos/benchmark/hue_filter_canvas/index.js
@@ -0,0 +1,38 @@
+function loadImage(src) {
+  const img = new Image();
+  return new Promise((resolve) => {
+    img.onload = function () {
+      resolve(img);
+    };
+    img.src = src;
+  });
+}
+
+const canvas = document.querySelector('canvas');
+
+(async function () {
+  const textureURL = 'https://p4.ssl.qhimg.com/t012170360e1552ce17.png';
+  const img = await loadImage(textureURL);
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+
+  function draw() {
+    const NUM = 100;
+    ctx.clearRect(0, 0, 512, 512);
+    for(let i = 0; i < NUM; i++) {
+      ctx.save();
+      ctx.filter = 'hue-rotate(180deg)';
+      ctx.translate(500 * Math.random(), 500 * Math.random());
+      ctx.beginPath();
+      ctx.rect(0, 0, 40, 40);
+      ctx.fill();
+      ctx.drawImage(img, 0, 0, 40, 40);
+      ctx.restore();
+    }
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+}());
---
+++ demos/benchmark/hue_filter_mesh_2d/index.js
@@ -0,0 +1,52 @@
+const {Renderer, Figure2D, Mesh2D} = meshjs;
+
+const canvas = document.querySelector('canvas');
+const renderer = new Renderer(canvas, {
+  contextType: '2d',
+});
+
+const textureURL = 'https://p4.ssl.qhimg.com/t012170360e1552ce17.png';
+const meshList = [];
+
+(async function () {
+  const texture = await renderer.loadTexture(textureURL);
+
+  const NUM = 1000;
+  const size = 40;
+
+  const figure = new Figure2D();
+  figure.rect(0, 0, size, size);
+
+  for(let i = 0; i < NUM; i++) {
+    const mesh = new Mesh2D(figure, canvas);
+
+    mesh.setFill({
+      color: [1, 0, 0, 0.5],
+    });
+
+    mesh.setTexture(texture, {
+      scale: false,
+      repeat: false,
+      rect: [0, 0, size, size],
+    });
+
+    mesh.hueRotate(180);
+
+    meshList.push(mesh);
+  }
+
+  function getData() {
+    return meshList.map((mesh) => {
+      mesh.setTransform(1, 0, 0, 1, 0, 0);
+      mesh.translate(500 * Math.random(), 500 * Math.random());
+      return mesh;
+    });
+  }
+
+  function update() {
+    const meshes = getData();
+    renderer.drawMeshes(meshes);
+    requestAnimationFrame(update);
+  }
+  update();
+}());
