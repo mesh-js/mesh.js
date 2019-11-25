bcc311e43b4e2fb865203d882f555b80b21b6f4d
akira-cn <akira.cn@gmail.com>
2019-11-25 19:47:03 +0800

    add node-canvas-webgl

--- examples/test-gl.js
+++ examples/test-gl.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const {createCanvas, loadImage} = require('node-canvas-webgl');
-const {Renderer, Figure2D, Mesh2D} = require('../dist/mesh');
 
+const {Renderer, Figure2D, Mesh2D} = require('../dist/mesh');
 const url = 'https://p0.ssl.qhimg.com/t01a72262146b87165f.png';
-const canvas = createCanvas(512, 512);
+
+const width = 512,
+  height = 512;
+
+const canvas = createCanvas(width, height);
 
 const f = new Figure2D();
 f.rect(0, 0, 100, 100);
@@ -11,12 +15,12 @@ f.rect(0, 0, 100, 100);
 const m = new Mesh2D(f, canvas);
 m.setFill({color: 'red'});
 
-const renderer = new Renderer(canvas);
+const renderer = new Renderer(canvas, {contextType: 'webgl'});
 
 loadImage(url).then((image) => {
   const texture = renderer.createTexture(image);
   m.setTexture(texture);
   renderer.drawMeshes([m]);
-  fs.writeFileSync('snap.png', canvas.toBuffer());
-  console.log(renderer);
+
+  fs.writeFileSync('snap2.png', canvas.toBuffer());
 });
