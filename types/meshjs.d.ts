3fbe6cf77d16168df9142f7c1f92cc0a44a752de
Rongjian Zhang <pd4d10@gmail.com>
2020-10-05 10:11:17 +0800

    add Figure2D proto methods

--- types/meshjs.d.ts
+++ types/meshjs.d.ts
@@ -19,6 +19,64 @@ interface Figure2DOptions {
 
 export class Figure2D {
   constructor(options?: string | Figure2DOptions);
+
+  readonly contours: Figure2DOptions[] | null;
+
+  /**
+   * Append a svg path to current Figure2D paths.
+   */
+  addPath(
+    /**
+     * a SVG Path String
+     */
+    path: string,
+  ): void;
+
+  /**
+   * Clear all paths in the Figure2D.
+   */
+  clear(): void;
+
+  /**
+   * Clear all paths in the Figure2D.
+   */
+  beginPath(): void;
+
+  arc(
+    x: number,
+    y: number,
+    radius: number,
+    startAngle: number,
+    endAngle: number,
+    anticlockwise?: 0 | 1,
+  ): void;
+
+  arcTo(
+    rx: number,
+    ry: number,
+    xAxisRotation: 0 | 1,
+    largeArcFlag: 0 | 1,
+    sweepFlag: 0 | 1,
+    x: number,
+    y: number,
+  ): void;
+
+  moveTo(x: number, y: number): void;
+
+  lineTo(x: number, y: number): void;
+
+  bezierCurveTo(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    x: number,
+    y: number,
+  ): void;
+
+  quadraticCurveTo(x1: number, y1: number, x: number, y: number): void;
+
+  rect(x: number, y: number, width: number, height: number): void;
 }
 
 export class Mesh2D {
